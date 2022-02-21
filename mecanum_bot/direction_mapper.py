0f464ddf0e4088d589b56e3ca5ef77c6c0fe0e26
gabrielgodoy17 <godoygabriel07@gmail.com>
2022-02-21 19:46:18 -0300

    Add WheelSpeed message and rework direction_mapper node

--- mecanum_bot/direction_mapper.py
+++ mecanum_bot/direction_mapper.py
@@ -2,38 +2,50 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+from mecanum_interfaces.msg import WheelSpeed
+
+POS_SPEED = 25.0
+NEG_SPEED = -25.0
+ZERO_SPEED = 0.0
+
 class DirectionMapper(Node):
 
     def __init__(self):
         super().__init__('direction_mapper')
         self.subscription = self.create_subscription(String, 'directions', self.listener_callback, 10)
         self.subscription
-        self.publisher_ = self.create_publisher(String, 'motors', 10)
+        self.publisher_ = self.create_publisher(WheelSpeed, 'motors', 10)
         
 
     def listener_callback(self, msg):
-        self.get_logger().info('Received: "%s"' % msg.data)
-        to_send = String()
-        to_send.data = dict.get(msg.data)
-        if to_send.data is None:
-            to_send.data = dict.get("STOP")
-        self.get_logger().info('Publishing: "%s"' % to_send)
+        self.get_logger().debug('Received: "%s"' % msg.data)
+        to_send = WheelSpeed()
+        to_send = dict.get(msg.data)
+        if to_send is None:
+            to_send = generate_msg(ZERO_SPEED, ZERO_SPEED, ZERO_SPEED, ZERO_SPEED)
+        self.get_logger().debug('Publishing: "%s"' % to_send)
         self.publisher_.publish(to_send)  
 
+def generate_msg(w1, w2, w3, w4):
+    msg = WheelSpeed()
+    msg.w1 = w1
+    msg.w2 = w2
+    msg.w3 = w3
+    msg.w4 = w4
+    return msg
 
 dict = {
-    "FWD" : "M1+,M2+,M3+,M4+",
-    "BKWD" : "M1-,M2-,M3-,M4-",
-    "L" : "M1-,M2+,M3+,M4-",
-    "R" : "M1+,M2-,M3-,M4+",
-    "FWD_L" : "M10,M2+,M3+,M40",
-    "FWD_R" : "M1+,M20,M30,M4+",
-    "BKWD_L" : "M1-,M20,M30,M4-",
-    "BKWD_R" : "M10,M2-,M3-,M40",
-    "CCW" : "M1+,M2-,M3+,M4-",
-    "CW" : "M1-,M2+,M3-,M4+",
-    "STOP" : "M10,M20,M30,M40",
-    "EXIT" : "EXIT"
+    "FWD" : generate_msg(POS_SPEED, POS_SPEED, POS_SPEED, POS_SPEED), #M1+,M2+,M3+,M4+
+    "BKWD" : generate_msg(NEG_SPEED, NEG_SPEED, NEG_SPEED, NEG_SPEED), #M1-,M2-,M3-,M4-
+    "L" : generate_msg(NEG_SPEED, POS_SPEED, POS_SPEED, NEG_SPEED), #M1-,M2+,M3+,M4-
+    "R" : generate_msg(POS_SPEED, NEG_SPEED, NEG_SPEED, POS_SPEED), #M1+,M2-,M3-,M4+
+    "FWD_L" : generate_msg(ZERO_SPEED, POS_SPEED, POS_SPEED, ZERO_SPEED), #M10,M2+,M3+,M40
+    "FWD_R" : generate_msg(POS_SPEED, ZERO_SPEED, ZERO_SPEED, POS_SPEED), #M1+,M20,M30,M4+
+    "BKWD_L" : generate_msg(NEG_SPEED, ZERO_SPEED, ZERO_SPEED, NEG_SPEED), #M1-,M20,M30,M4-
+    "BKWD_R" : generate_msg(ZERO_SPEED, NEG_SPEED, NEG_SPEED, ZERO_SPEED), #M10,M2-,M3-,M40
+    "CCW" : generate_msg(POS_SPEED, NEG_SPEED, POS_SPEED, NEG_SPEED), #M1+,M2-,M3+,M4-
+    "CW" : generate_msg(NEG_SPEED, POS_SPEED, NEG_SPEED, POS_SPEED), #M1-,M2+,M3-,M4+
+    "STOP" : generate_msg(ZERO_SPEED, ZERO_SPEED, ZERO_SPEED, ZERO_SPEED), #M10,M20,M30,M40
 }
 
 def main(args=None):
@@ -41,7 +53,7 @@ def main(args=None):
     rclpy.init(args=args)
 
     direction_mapper = DirectionMapper()
-
+    direction_mapper.get_logger().info("Direction Mapper ready")
     rclpy.spin(direction_mapper)
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
