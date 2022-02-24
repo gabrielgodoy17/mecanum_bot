a0934ff2759e194be45c14b81833683ec2c9f181
gabrielgodoy17 <godoygabriel07@gmail.com>
2022-02-24 19:49:05 -0300

    Fix regex

--- mecanum_bot/spi_interface.py
+++ mecanum_bot/spi_interface.py
@@ -21,8 +21,8 @@ spi.open(0,0)
 spi.max_speed_hz=244000
 spi.mode = 0b00
 
-regex_w1 = re.compile(r'(?<=w1\+)(\d{2})|(?<=w1)(\d{3})|(?<=w1-)(\d{2})')
-regex_w2 = re.compile(r'(?<=w2\+)(\d{2})|(?<=w2)(\d{3})|(?<=w2-)(\d{2})')
+regex_w1 = re.compile(r'(?<=w1)(\+\d{2})|(?<=w1)(\d{3})|(?<=w1)(-\d{2})')
+regex_w2 = re.compile(r'(?<=w2)(\+\d{2})|(?<=w2)(\d{3})|(?<=w2)(-\d{2})')
 
 class SpiInterface(Node):
 
@@ -31,7 +31,7 @@ class SpiInterface(Node):
 		self.subscription = self.create_subscription(WheelSpeed, 'motors', self.listener_callback, 10)
 		self.subscription #prevent unused variable warning
 		self.publisher_ = self.create_publisher(WheelSpeed, 'wheels_speed', 10)
-		timer_period = 1  # seconds   
+		timer_period = 0.05  # seconds   
 		self.timer = self.create_timer(timer_period, self.timer_callback)
 		self.i = 0
 		self.to_send_slave1 = ":w1000;:w2000;" #intialize with wheels stopped
@@ -40,35 +40,26 @@ class SpiInterface(Node):
 	def timer_callback(self):
 		msg = WheelSpeed()
 
-		#to_send_slave1 = dict.get(to_send_spi[0:3]) + dict.get(to_send_spi[4:7])
 		#Slave 1 spi
-		self.get_logger().info("to_send_slave1: "+self.to_send_slave1)
+		self.get_logger().debug("to_send_slave1: "+self.to_send_slave1)
 		slave_select_1.off()
 		response = spi.xfer2(bytearray(self.to_send_slave1.encode(encoding='UTF-8')))
 		#Process slave 1 response
 		slave_1 = ''.join([str(chr(elem)) for elem in response])
+		self.get_logger().debug("recieve from slave1: "+slave_1)
 		w1 = regex_w1.search(slave_1)
-		self.get_logger().info("recieve from slave1: "+slave_1)
-		self.get_logger().info("w1: (only nums)"+w1.group())
-		self.get_logger().info("w1: (float()) %f" %float(w1.group()))
 		w2 = regex_w2.search(slave_1)
-		self.get_logger().info("w2: (only nums)"+w2.group())
-		self.get_logger().info("w2: (float()) %f" %float(w2.group()))
 		slave_select_1.on()
 
 		#Slave 2 spi
-		self.get_logger().info("to_send_slave2: "+self.to_send_slave2)
+		self.get_logger().debug("to_send_slave2: "+self.to_send_slave2)
 		slave_select_2.off()
 		response2 = spi.xfer2(bytearray(self.to_send_slave2.encode(encoding='UTF-8')))		
 		#Process slave 2 response
 		slave_2 = ''.join([str(chr(elem)) for elem in response2])
-		self.get_logger().info("recieve from slave2: "+slave_2)
+		self.get_logger().debug("recieve from slave2: "+slave_2)
 		w3 = regex_w1.search(slave_2)
-		self.get_logger().info("w3: (only nums)"+w3.group())
-		self.get_logger().info("w3: (float()) %f" %float(w3.group()))
 		w4 = regex_w2.search(slave_2)
-		self.get_logger().info("w4: (only nums)"+w4.group())
-		self.get_logger().info("w4: (float()) %f" %float(w4.group()))
 		slave_select_2.on()
 		
 		if w1 and w2 and w3 and w4:
@@ -77,21 +68,18 @@ class SpiInterface(Node):
 			msg.w3 = float(w3.group())
 			msg.w4 = float(w4.group())
 			self.publisher_.publish(msg)
-			self.get_logger().info('Publishing: "%s"' % msg)
+			self.get_logger().debug('Publishing: "%s"' % msg)
 
 	def listener_callback(self, msg):
 
-		self.get_logger().info('Received from topic: %s' % msg)
+		self.get_logger().debug('Received from topic: %s' % msg)
 
 		#Message slave 1
-
 		self.to_send_slave1 = generate_command(msg.w1, 1,self) + generate_command(msg.w2, 2,self)
-		self.get_logger().info('Listener Callback slave 1: ' + self.to_send_slave1)
 
 		#Message slave 2
-
 		self.to_send_slave2 = generate_command(msg.w3, 1,self) + generate_command(msg.w4, 2,self)
-		self.get_logger().info('Listener Callback slave 2: ' + self.to_send_slave2)
+
 
 def generate_command(wheel_speed, wheel_num,self):
 	wheel_speed_int = int(round(wheel_speed))
@@ -108,7 +96,6 @@ def generate_command(wheel_speed, wheel_num,self):
 	elif len(wheel_speed_string) > 2: #Should never happen, just in case
 		sign = "0"
 		wheel_speed_string = "00"
-	self.get_logger().info('generated command : ' + ":w" + str(wheel_num) + sign + wheel_speed_string + ";")
 
 	return ":w" + str(wheel_num) + sign + wheel_speed_string + ";"
 
