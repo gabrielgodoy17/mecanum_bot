b97c70c7fb0f0f19720264ff1cc6bd0b2af5b276
gabrielgodoy17 <godoygabriel07@gmail.com>
2022-04-05 19:29:11 -0300

    Remove all excess nodes

--- launch/mecanum_bot_lite.launch.py
+++ launch/mecanum_bot_lite.launch.py
@@ -14,44 +14,6 @@ def generate_launch_description():
     ydlidar_params_file = os.path.join(ydlidar_pkg_share, 'params/ydlidar.yaml')
     mpu_params_file = os.path.join(mpu_pkg_share, 'params/mpu9250.yaml')
 
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}],
-        arguments=[default_model_path]
-    )
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'source_list': ['wheels_joint_state']}]
-    )
-
-    direction_mapper_node = Node(
-        package='mecanum_bot',
-        executable='direction_mapper',
-        name='direction_mapper'
-    )
-
-    fwd_kinematics_node = Node(
-        package='mecanum_bot',
-        executable='fwd_kinematics',
-        name='fwd_kinematics'
-    )
-
-    inv_kinematics_node = Node(
-        package='mecanum_bot',
-        executable='inv_kinematics',
-        name='inv_kinematics'
-    )
-
-    wheels_joint_update_node = Node(
-        package='mecanum_bot',
-        executable='wheels_joint_update',
-        name='wheels_joint_update'
-    )
-
     spi_interface_node = Node(
         package='mecanum_bot',
         executable='spi_interface',
@@ -64,13 +26,6 @@ def generate_launch_description():
         name='ydlidar_node',
         emulate_tty=True,
         parameters=[LaunchConfiguration('ydlidar_params_file')],
-    )
-
-    tf2_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_tf_pub_laser',
-        arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser_frame'],
     )
 
     mpu_node = Node(
@@ -95,13 +50,6 @@ def generate_launch_description():
             name='mpu_params_file',
             default_value=mpu_params_file,
             description='Path to the ROS2 parameters file to use.'),
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        direction_mapper_node,
-        fwd_kinematics_node,
-        inv_kinematics_node,
-        wheels_joint_update_node,
         spi_interface_node,
-        ydlidar_node,
-        tf2_node
+        ydlidar_node
     ])
