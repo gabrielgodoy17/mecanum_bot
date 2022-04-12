567342d151e4b1194ddfac8d62340d3a703bfced
gabrielgodoy17 <godoygabriel07@gmail.com>
2022-04-12 20:37:45 -0300

    deprecating timer_callback

--- mecanum_bot/spi_interface.py
+++ mecanum_bot/spi_interface.py
@@ -29,9 +29,9 @@ class SpiInterface(Node):
 		self.subscription = self.create_subscription(WheelSpeed, 'motors', self.listener_callback, 10)
 		self.subscription #prevent unused variable warning
 		self.publisher_ = self.create_publisher(String, 'wheels_speed', 10)
-		timer_period = 0.005  # seconds   
-		self.timer = self.create_timer(timer_period, self.timer_callback)
-		self.i = 0
+		# timer_period = 0.005  # seconds   
+		# self.timer = self.create_timer(timer_period, self.timer_callback)
+		# self.i = 0
 		self.to_send_slave1 = ":w1000;:w2000;" #intialize with wheels stopped
 		self.to_send_slave2 = ":w3000;:w4000;" #intialize with wheels stopped
 
@@ -104,6 +104,34 @@ class SpiInterface(Node):
 
 		#Message slave 2
 		self.to_send_slave2 = generate_command(msg.w3, 1,self) + generate_command(msg.w4, 2,self)
+		
+		msg = WheelSpeed()
+
+		#Slave 1 spi
+		#self.get_logger().debug("to_send_slave1: "+self.to_send_slave1)
+		slave_select_1.off()
+		response = spi.xfer2(bytearray(self.to_send_slave1.encode(encoding='UTF-8')))
+		slave_1 = ''.join([str(chr(elem)) for elem in response])
+		slave_1 = slave_1.replace(":","")
+		slave_1 = slave_1.replace(";","")
+		#self.get_logger().debug(slave_1)
+		slave_select_1.on()
+
+		#Slave 2 spi
+		#self.get_logger().debug("to_send_slave2: "+self.to_send_slave2)
+		slave_select_2.off()
+		response2 = spi.xfer2(bytearray(self.to_send_slave2.encode(encoding='UTF-8')))		
+		slave_2 = ''.join([str(chr(elem)) for elem in response2])
+		slave_2 = slave_2[1:2] + "3" + slave_2[3:6] + slave_2[8:9] + "4" + slave_2[10:13]
+		#self.get_logger().debug(slave_2)
+		slave_select_2.on()
+
+		to_send_pub = slave_1 + slave_2
+
+		msg = String()
+		msg.data= to_send_pub
+		self.publisher_.publish(msg)
+		#self.get_logger().debug('Publishing: "%s"' % msg.data)
 
 
 def generate_command(wheel_speed, wheel_num,self):
